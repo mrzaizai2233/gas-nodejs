1bf79f83f4e65397b3dea29b7b4ad291d2c63e72
mrzaizai2233 <kiepsongthua18@gmail.com>
2017-12-12 17:40:50 +0700

    12/12

--- app/controllers/OrderController.js
+++ app/controllers/OrderController.js
@@ -2,10 +2,18 @@ const Order = require('../models/order');
 const OrderDetail = require('../models/order_detail');
 
 exports.orders = function(req, res) {
-    Order.find().populate('items').exec(function(err, order) {
-        if (err) res.send(err);
-        res.send(order);
-    })
+    Order.find()
+        .populate({
+            path: 'items',
+            populate: {
+                path: 'product'
+            }
+        })
+        .populate('user')
+        .exec(function(err, order) {
+            if (err) res.send(err);
+            res.send(order);
+        })
 }
 
 exports.order_create = function(req, res) {
@@ -18,39 +26,48 @@ exports.order_create = function(req, res) {
     order.subtotal = req.body.subtotal;
     order.status = 1;
     var items = [];
-
     req.body.items.forEach(function(item, index) {
-        let order_detail = new OrderDetail();
-        order_detail.product = item.product;
-        order_detail.qty = item.qty;
-        order_detail.price = item.price;
-        order_detail.discout = item.discout;
-        order_detail.discout_total = item.discout_total;
-        order_detail.note = item.note;
-        order_detail.save(function(err, data) {
-            items.push(data._id);
-            console.log(data._id)
-        })
-    });
-    console.log(items)
-    order.save(function(err, order) {
-        // order.items = [];
-        // items.forEach(function(item, index) {
-        //     item.order = order._id;
-        //     item.save(function(err, order_detail) {
-        //         // console.log(order_detail._id)
-        //         order.items.push(order_detail._id);
-        //         console.log(order.items)
+        items.push(new Promise(function(resolve, reject) {
 
-        //     });
-        // })
-        // order.save();
-        res.send("ok")
-
+            let order_detail = new OrderDetail();
+            order_detail.product = item.product;
+            order_detail.qty = item.qty;
+            order_detail.price = item.price;
+            order_detail.discout = item.discout;
+            order_detail.discout_total = item.discout_total;
+            order_detail.note = item.note;
+            order_detail.save(function(err, data) {
+                resolve(data._id)
+            });
+        }))
     })
+    Promise.all(items).then(data => {
+        order.items = data;
+        order.save(function(err, order) {
+            res.send(order)
+        })
+    })
+
 }
 exports.order_update = function(req, res) {}
-exports.order_delete = function(req, res) {}
+exports.order_delete = function(req, res) {
+    Order.find({ _id: req.params.id }).populate('items').exec(function(err, order) {
+
+        if (order.items) {
+            order.items.forEach(function(order_detail) {
+                OrderDetail.findByIdAndRemove(order_detail, function(err) {
+                    console.log("delete successs");
+                });
+            })
+        }
+    })
+
+    Order.findByIdAndRemove({ _id: req.params.id }, function(err, order) {
+        res.send(order._id);
+    });
+
+
+}
 exports.order = function() {
 
 }
--- app/models/order.js
+++ app/models/order.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+
 const order = Schema({
     code: String,
     create_at: Date,
