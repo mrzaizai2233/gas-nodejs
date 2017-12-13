8fe55b93cf45acd5e7e31622e72176852fde19f7
mrzaizai2233 <kiepsongthua18@gmail.com>
2017-12-13 17:41:02 +0700

    13/12

--- app/controllers/OrderController.js
+++ app/controllers/OrderController.js
@@ -27,37 +27,39 @@ exports.order_create = function(req, res) {
     order.grand_total = req.body.grand_total;
     order.subtotal = req.body.subtotal;
     order.status = req.body.status;
-    var items=[];
-    req.body.items.forEach(function(item,index) {
-        items.push(new Promise(function(resovel,rej){
+
+    console.log(order._id);
+
+    var items = [];
+    req.body.items.forEach(function(item, index) {
+        items.push(new Promise(function(resovel, rej) {
             let order_detail = new OrderDetail();
             order_detail.product = item.product;
-            order_detail.qty =item.qty;
+            order_detail.qty = item.qty;
             order_detail.price = item.price;
-            order_detail.discout= item.discout;
-            order_detail.discout_total= item.discout_total;
+            order_detail.discout = item.discout;
+            order_detail.discout_total = item.discout_total;
             order_detail.note = item.note;
-            order_detail.save(function(err,res){
+            order_detail.order = order._id;
+            order_detail.save(function(err, res) {
                 resovel(res._id)
             })
         }))
     });
-    Promise.all(items).then(items=>{
-        order.items= items;
-        order.save(function(err,data){
+    Promise.all(items).then(items => {
+        order.items = items;
+        order.save(function(err, data) {
             res.send(data);
         });
-
     })
-    
+
 }
 exports.order_update = function(req, res) {}
 exports.order_delete = function(req, res) {
     Order.find({ _id: req.params.id }).populate('items').exec(function(err, order) {
         if (order.items) {
             order.items.forEach(function(order_detail) {
-                OrderDetail.findByIdAndRemove(order_detail, function(err) {
-                });
+                OrderDetail.findByIdAndRemove(order_detail._id, function(err) {});
             })
         }
     })
