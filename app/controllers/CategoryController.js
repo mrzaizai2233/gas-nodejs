7bae859e342102f16be6bbde35e2390c1125407c
tuandat <kiepsongthuadt@gmail.com>
2018-02-10 07:00:48 +0700

    123

--- app/controllers/CategoryController.js
+++ app/controllers/CategoryController.js
@@ -9,6 +9,7 @@ exports.category_list = function(req,res){
 
 exports.category_create = function(req,res){
         var category = new categoryModel();
+        console.log(req.body)
         category.name = req.body.name;
         category.status = req.body.status;
         category.save(function(err,data) {
@@ -31,6 +32,20 @@ exports.category_update = function(req,res){
 exports.category_delete = function(req,res){
     categoryModel.findByIdAndRemove(req.params.id,function(err,result){
         if(err) res.send(err)
-        res.send(result)
+        res.send(result._id)
     })
+}
+
+exports.change_status = function(req,res){
+    try {
+        categoryModel.findById(req.body._id).exec(function(err,result){
+            result.status = !result.status;
+            result.save(function(err,data){
+                res.send(data._id)
+            })
+        });
+    } catch(err){
+        res.send(err)
+    }
+   
 }
--- app/routers/categoryRouter.js
+++ app/routers/categoryRouter.js
@@ -4,5 +4,6 @@ router.get('/categorys',categoryController.category_list)
 router.post('/category/create',categoryController.category_create)
 router.post('/category/update',categoryController.category_update)
 router.get('/category/delete/:id',categoryController.category_delete)
+router.post('/category/change_status',categoryController.change_status)
 
 module.exports=router;
